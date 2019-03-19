ba46ba5eed82fd9c611da76b4a61e3539ca7cfca
GaoZhenGit <912815282@qq.com>
2019-03-19 19:43:08 +0800

    write configure script

---
+++ configure.sh
@@ -0,0 +1,24 @@
+export NDK=/home/gz/Android/android-ndk-r17c
+
+export API=14
+export NDK=/home/gz/Android/android-ndk-r17c
+export SYSROOT=$NDK/platforms/android-$API/arch-arm/
+export TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
+export PREFIX=./output
+./configure \
+--prefix=$PREFIX \
+--enable-shared \
+--enable-static \
+--disable-doc \
+--disable-ffplay \
+--disable-ffprobe \
+--disable-symver \
+--disable-ffmpeg \
+--cc=$TOOLCHAIN/bin/arm-linux-androideabi-gcc \
+--cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+--target-os=linux \
+--arch=arm \
+--enable-cross-compile \
+--sysroot=$SYSROOT \
+--extra-cflags="-I$NDK/sysroot/usr/include/arm-linux-androideabi -isysroot $NDK/sysroot -fPIC -DANDROID -D__thumb__ -mthumb -Wfatal-errors -Wno-deprecated -mfloat-abi=softfp -marm -march=armv7-a" \
+--enable-neon
